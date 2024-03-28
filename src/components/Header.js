378f84a02bdece0d076ce144a286f48bc86ef8e2
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-03-27 23:17:00 -0700

    Added a hamburger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../imgs/Asset 16@4x.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen((open) => !open);
+	};
 	return (
 		<header>
 			<nav>
 				<div className="wrapper">
 					<div className="header-container">
-						<div className="img-container">
-							<img src={logo} alt="logo" />
-						</div>
-						{/* <ul>
+						<img src={logo} alt="logo" />
+						{/* <span className="menu" onClick={toggleMenu}>
+							Trigger
+						</span> */}
+						<FontAwesomeIcon
+							icon={faBars}
+							className="menu"
+							onClick={toggleMenu}
+						/>
+						<ul className={`${isOpen ? "is-open" : ""}`}>
 							<li>Home</li>
 							<li>About</li>
 							<li>Menu</li>
 							<li>Reservations</li>
 							<li>Order Online</li>
 							<li>Login</li>
-						</ul> */}
+						</ul>
 					</div>
 				</div>
 			</nav>
